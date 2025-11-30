cef6d260b173b57ca065f0547416e3f712d6d152
Plata101 <marc.friedmann@yahoo.com>
2025-11-30 19:09:14 +0100

    Scenes added

---
+++ scripts/GameManager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# Spieler-Daten
+var currentGold: int = 100
+var playerHp: int = 100
+var playerMaxHp: int = 100
+
+# Spiel-Status
+var currentDay: int = 1
+var maxDays: int = 100
+
+# TODO: Inventar-Variablen (Tränke etc.)
